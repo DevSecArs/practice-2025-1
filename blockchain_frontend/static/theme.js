29c6d3750859c3275afa9036e1602912d1dd8307
PyArslan <agaev.arslan.com01@gmail.com>
2025-05-14 02:58:41 +0300

    Добавлено лого, footer, контент для страницы about, Проверка на минимальную длину пароля при регистрации, Улучшен дизайн страницы и подправлены мелкие ошибки

--- blockchain_frontend/static/theme.js
+++ blockchain_frontend/static/theme.js
@@ -1,13 +1,21 @@
 function toggleTheme() {
     const body = document.body;
     const navbar = document.getElementById('navbar');
+    const footer = document.getElementById('footer');
+
     body.classList.toggle('dark-theme');
     if (body.classList.contains('dark-theme')) {
-        navbar.classList.remove('navbar-light', 'bg-light');
-        navbar.classList.add('navbar-dark', 'bg-dark');
+        navbar.classList.remove('navbar-light', 'bg-light', 'border-dark');
+        footer.classList.remove('navbar-light', 'bg-light', 'border-dark');
+
+        navbar.classList.add('navbar-dark', 'bg-dark', 'border-light');
+        footer.classList.add('navbar-dark', 'bg-dark', 'border-light');
     } else {
-        navbar.classList.remove('navbar-dark', 'bg-dark');
-        navbar.classList.add('navbar-light', 'bg-light');
+        navbar.classList.remove('navbar-dark', 'bg-dark', 'border-light');
+        footer.classList.remove('navbar-dark', 'bg-dark', 'border-light');
+
+        navbar.classList.add('navbar-light', 'bg-light', 'border-dark');
+        footer.classList.add('navbar-light', 'bg-light', 'border-dark');
     }
     const theme = body.classList.contains('dark-theme') ? 'dark' : 'light';
     localStorage.setItem('theme', theme);
@@ -22,9 +30,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('theme');
     const body = document.body;
     const navbar = document.getElementById('navbar');
+    const footer = document.getElementById('footer');
     if (savedTheme === 'dark') {
         body.classList.add('dark-theme');
-        navbar.classList.remove('navbar-light', 'bg-light');
-        navbar.classList.add('navbar-dark', 'bg-dark');
+        navbar.classList.remove('navbar-light', 'bg-light', 'border-dark');
+        footer.classList.remove('navbar-light', 'bg-light', 'border-dark');
+
+        navbar.classList.add('navbar-dark', 'bg-dark', 'border-light');
+        footer.classList.add('navbar-dark', 'bg-dark', 'border-light');
     }
 });
